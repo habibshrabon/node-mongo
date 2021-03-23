49aca850c5d98cb5fe39353a019d523c65b6930f
habibshrabon <snhabib777@gmail.com>
2021-03-23 09:27:55 +0600

    some work added

--- index.js
+++ index.js
@@ -3,8 +3,22 @@ const express = require('express');
 
 const app = express();
 
+const users = ['Jabir', 'Lopa', 'Fatema', 'Mim', 'Reza']
+
 app.get('/', (req, res) => {
-    res.send("Thank you for calling me");
+    res.send("I have another idea");
+});
+
+app.get('/fruits/banana', (req, res) => {
+    res.send({fruit: 'banana', quantity: 1000, price:10000});
 })
 
-app.listen(3000, () => console.log('Listening to port 3000'));
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const name = users[id];
+    res.send({id, name})
+})
+
+
+app.listen(4200, () => console.log('Listening to port 4200'));
