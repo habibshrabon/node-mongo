ac4eec85657c364670db471ed8006ece1d3b492d
habibshrabon <snhabib777@gmail.com>
2021-03-24 09:21:06 +0600

    Module 45 study is complete

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 
 const app = express();
 
+app.use(cors());
+app.use(bodyParser.json());
+
 const users = ['Jabir', 'Lopa', 'Fatema', 'Mim', 'Reza']
 
 app.get('/', (req, res) => {
-    res.send("I have another idea");
+    res.send("I have another idea and best");
 });
 
 app.get('/fruits/banana', (req, res) => {
@@ -20,5 +25,13 @@ app.get('/users/:id', (req, res) => {
     res.send({id, name})
 })
 
+
+//post
+app.post('/addUser', (req, res) => {
+    //save to database
+    const user = req.body;
+    user.id = 44;
+    res.send(req.body);
+})
 
 app.listen(4200, () => console.log('Listening to port 4200'));
